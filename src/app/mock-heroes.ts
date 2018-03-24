d1c4493145a573bbadcf4b0170c7117eb57e8835
AndrewMcShane <andrewjohnmcshane@gmail.com>
2018-03-23 19:25:19 -0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Iron Man' },
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'The Flash' },
+  { id: 14, name: 'Super Man' },
+  { id: 15, name: 'Captain America' },
+  { id: 16, name: 'Hulk' },
+  { id: 17, name: 'Wonder Woman' },
+  { id: 18, name: 'Green Lantern' },
+  { id: 19, name: 'Catwoman' },
+  { id: 20, name: 'Cyborg' }
+];
