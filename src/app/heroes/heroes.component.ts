8ec6c8a795a4ec6809d2cedb1a91051daa12af5d
AndrewMcShane <andrewjohnmcshane@gmail.com>
2018-03-26 12:59:27 -0500

    Nearly complete, http left

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero }              from '../hero';
-import { HEROES }            from '../mock-heroes';
+import { HeroService}        from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,17 +9,16 @@ import { HEROES }            from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero : Hero;
+  heroes: Hero[];
 
-  onSelect(hero : Hero): void {
-    this.selectedHero = hero;
-  }
-
-  heroes = HEROES;
-
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   //a good place to put initialization logic (akin to the Start() method in Unity C# code.).
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
